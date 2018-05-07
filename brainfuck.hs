879fe0ccbb57a44c58c76949ca9b57a1851bcda2
Nicholas <dojoman19@gmail.com>
2018-05-06 23:41:52 -0400

    Finished rough implementation but still many bugs

--- brainfuck.hs
+++ brainfuck.hs
@@ -16,11 +16,27 @@ moveRight :: Tape a -> Tape a
 moveRight (Tape ls m (r:rs)) = Tape (ls ++ [m]) r rs
 
 moveRightInst :: Tape Char -> Tape Char 
-moveRightInst tape@(Tape ls m (r:rs)) = moveRight tape 
 moveRightInst (Tape ls m []) = Tape (ls ++ [m]) '0' []
+moveRightInst tape@(Tape ls m (r:rs)) = moveRight tape 
 
 moveLeft :: Tape a -> Tape a
 moveLeft (Tape (l:ls) m rs) = Tape ls l (m:rs)
+
+seekLeft inst@(Tape (l:ls) m _)
+    | l == '[' = inst
+    | otherwise = seekLeft (moveLeft inst)
+
+seekRight inst@(Tape (l:ls) m _)
+    | l == ']' = inst 
+    | otherwise = seekRight (moveRightInst inst)
+
+doFunc dataTape@(Tape _ m _) inst@(Tape _ '[' _)
+    | m == chr 0 = doFunc dataTape (seekRight inst) 
+    | otherwise = doFunc dataTape (moveRightInst inst)
+
+doFunc dataTape@(Tape _ m _) inst@(Tape _ ']' _) 
+    | m /= chr 0 = doFunc dataTape (seekLeft inst)
+    | otherwise = doFunc dataTape (moveRightInst inst)
 
 --End condition
 doFunc dataTape inst@(Tape _ '0' _) = return ()
@@ -37,11 +53,11 @@ doFunc dataTape inst@(Tape _ '<' _) =
     doFunc (moveLeft dataTape) (moveRightInst inst)
 
 doFunc dataTape inst@(Tape _ '>' _) = 
-    doFunc (moveRight dataTape) (moveRightInst inst)
+    doFunc (moveRight dataTape) (moveRightInst inst) 
 
 doFunc dataTape@(Tape _ m _) inst@(Tape _ '.' _) = do 
     putChar(m)
-    doFunc dataTape (moveRightInst inst)
+    doFunc dataTape (moveRightInst inst) 
 
 doFunc dataTape@(Tape ls _ rs) inst@(Tape _ ',' _) = do 
     m <- getChar 
